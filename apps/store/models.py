2a0ff7f52c73acf154999d06fe9f5b80d422ee86
Alyson Takai <shtakai@gmail.com>
2016-05-31 10:39:16 -0700

    added Order and Cart model

--- apps/store/models.py
+++ apps/store/models.py
@@ -21,3 +21,27 @@ class Manufacturer(models.Model):
 
     class Meta:
         db_table = 'manufacturers'
+
+
+class Order(models.Model):
+    shipping_status = models.BooleanField(default=False)
+    shipping_name = models.CharField(max_length=50)
+    shipping_address = models.CharField(max_length=255)
+    card_number = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'orders'
+
+
+class Cart(models.Model):
+    product = models.ForeignKey('Product')
+    order = models.ForeignKey('Order')
+    amount = models.PositiveSmallIntegerField(default=0)
+    unit_price = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'carts'
